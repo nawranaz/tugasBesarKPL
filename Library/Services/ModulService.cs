7ce2bc36503442564b6bf236500f2c50772e4ffb
tahtafa <tahtaccnt@gmail.com>
2025-05-17 23:28:01 +0700

    Add library project

---
+++ Library/Services/ModulService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace TubesKPL_KitaBelajar.Library.Services
+{
+    public static class ModulService
+    {
+        public static List<T> LoadData<T>(string fileName)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", fileName);
+
+            if (!File.Exists(filePath))
+                return new List<T>();
+
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<T>>(json);
+        }
+    }
+}
