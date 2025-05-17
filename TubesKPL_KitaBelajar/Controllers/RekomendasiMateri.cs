f88d39e783bbf3f7f704d1c34f14771b22b0a445
SummaryRex <139709884+SummaryRex@users.noreply.github.com>
2025-05-17 15:35:24 +0700

    nambahin rekomendasi materi

---
+++ TubesKPL_KitaBelajar/Controllers/RekomendasiMateri.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TubesKPL_KitaBelajar.Model;
+
+namespace TubesKPL_KitaBelajar.Controllers
+{
+    internal class RekomendasiMateri
+    {
+        private readonly List<UserProgress> _progressList;
+
+        public RekomendasiMateri(List<UserProgress> progressList)
+        {
+            // Defensive: pastikan list tidak null
+            _progressList = progressList ?? new List<UserProgress>();
+        }
+
+        public List<string> GetRekomendasi()
+        {
+            var rekomendasi = new List<string>();
+
+            foreach (var progress in _progressList)
+            {
+                // Defensive: validasi setiap data
+                if (string.IsNullOrEmpty(progress.NamaMateri))
+                    continue;
+
+                if (progress.Nilai < 70) // misalnya batas lulus 70
+                {
+                    rekomendasi.Add(progress.NamaMateri);
+                }
+            }
+
+            return rekomendasi;
+        }
+    }
+}
---
+++ TubesKPL_KitaBelajar/Model/UserProgress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TubesKPL_KitaBelajar.Model
+{
+    internal class UserProgress
+    {
+        public string NamaMateri { get; set; }
+        public int Nilai { get; set; }
+    }
+}
