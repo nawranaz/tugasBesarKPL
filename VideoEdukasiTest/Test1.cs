74d3f8b335fa86aaf44b16c468b7f59120c72289
SummaryRex <139709884+SummaryRex@users.noreply.github.com>
2025-05-20 04:37:33 +0700

    video edukasi tes

---
+++ VideoEdukasiTest/Test1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using TubesKPL_KitaBelajar.Controllers;
+
+namespace VideoEdukasiTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TampilkanVideo()
+        {
+            var simulatedInput = new StringReader(
+                "1\n" +
+                "1\n" +
+                "1\n"
+            );
+
+            var output = new StringWriter();
+
+            Console.SetIn(simulatedInput);
+            Console.SetOut(output);
+
+            VideoEdukasi.RunVideo();
+
+            string hasilOutput = output.ToString();
+
+            Assert.IsTrue(hasilOutput.Contains("Daftar video untuk kategori"));
+            Assert.IsTrue(hasilOutput.Contains("Memutar"));
+
+        }
+    }
+}
