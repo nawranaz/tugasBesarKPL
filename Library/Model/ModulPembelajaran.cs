c34f85de39a1f61f4a315b1aceb891da8e715ab8
tahtafa <tahtaccnt@gmail.com>
2025-05-19 21:05:16 +0700

    add testing for modul and modul search

--- Library/Model/ModulPembelajaran.cs
+++ Library/Model/ModulPembelajaran.cs
@@ -4,9 +4,10 @@ namespace TubesKPL_KitaBelajar.Library.Model
 {
     public class ModulPembelajaran
     {
-        public string MataPelajaran { get; set; }
-        public string Judul { get; set; }
-        public string Deskripsi { get; set; }
-        public string Konten { get; set; }
+        public string MataPelajaran { get; set; } = string.Empty;
+        public string Judul { get; set; } = string.Empty;
+        public string Deskripsi { get; set; } = string.Empty;
+        public string Konten { get; set; } = string.Empty;
     }
+
 }
